026e77998493801ffb69740a9cb6bdc7e76af774
Sara Gibbons <saragibby@gmail.com>
2020-04-20 22:10:00 -0400

    🐓

---
+++ sendSms.js
@@ -0,0 +1,11 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+client.messages
+  .create({
+    body: "Hello from Message Rooster",
+    from: process.env.TWILIO_FROM_NUMBER,
+    to: "+15558675309"
+  })
+  .then(message => console.log(message.sid));
