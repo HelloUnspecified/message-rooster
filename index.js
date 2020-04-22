4304c21a9acebad9724c1df04cebb00a4665ca0d
Sara Gibbons <saragibby@gmail.com>
2020-04-22 19:50:49 -0400

    figuring out the bits

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.options("*", cors());
 
 app.get("/", (req, res) => res.send("Working!!!"));
 
+app.post("/sms", (req, res) => {
+  console.log(`Incoming message from ${req}: ${res}`);
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end("BACON");
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log("server running on port 3000", "");
 });
