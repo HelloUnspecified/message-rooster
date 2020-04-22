b30d2e6b8d721a865b8d05a1338cfb7eccb51f98
Sara Gibbons <saragibby@gmail.com>
2020-04-22 17:46:22 -0400

    get server fired up

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const http = require("http");
 const express = require("express");
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.post("/sms", (req, res) => {
+  console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
   const twiml = new MessagingResponse();
 
-  twiml.message("Great to hear from you! ❤️ Message Rooster");
+  twiml.message("Thanks for reaching out to Unspecified! We ❤️ connecting with people and making awesome happen! What can we help you with today?");
 
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
