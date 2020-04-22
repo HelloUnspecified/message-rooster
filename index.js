ba74b6a99dad903f8a31ee518067430932e29080
Sara Gibbons <saragibby@gmail.com>
2020-04-22 19:50:49 -0400

    figuring out the bits

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const http = require("http");
 const express = require("express");
 const app = express();
-var cors = require("cors");
+const bodyParser = require("body-parser");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(
   cors({
@@ -14,10 +19,17 @@ app.get("/", (req, res) => res.send("Working!!!"));
 
 app.post("/sms", (req, res) => {
   console.log(`Incoming message from ${req}: ${res}`);
+  // console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
+  const twiml = new MessagingResponse();
+
+  twiml.message(
+    "Thanks for reaching out to Unspecified! We ❤️ connecting with people and making awesome happen! What can we help you with today?"
+  );
+
   res.writeHead(200, { "Content-Type": "text/xml" });
-  res.end("BACON");
+  res.end(twiml.toString());
 });
 
-app.listen(process.env.PORT || 3000, function () {
+http.createServer(app).listen(process.env.PORT || 3000, function () {
   console.log("server running on port 3000", "");
 });
