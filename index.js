634fa325970ac39d23c9435786a8a785f6a1a7a9
Sara Gibbons <saragibby@gmail.com>
2020-04-22 20:09:12 -0400

    wire up notification to admins

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+
 const http = require("http");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
 const cors = require("cors");
+const sendSms = require('./sendSms');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -18,8 +21,9 @@ app.options("*", cors());
 app.get("/", (req, res) => res.send("Working!!!"));
 
 app.post("/sms", (req, res) => {
-  console.log(`Incoming message from ${req}: ${res}`);
-  // console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
+  const messageBody = req.body.Body;
+  // console.log(`Incoming message from ${req}: ${res}`);
+  console.log(`Incoming message from ${req.body.From}: ${messageBody}`);
   const twiml = new MessagingResponse();
 
   twiml.message(
@@ -28,6 +32,8 @@ app.post("/sms", (req, res) => {
 
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
+
+  sendSms.notifyAdmins({ messageBody });
 });
 
 http.createServer(app).listen(process.env.PORT || 3000, function () {
--- sendSms.js
+++ sendSms.js
@@ -1,13 +1,22 @@
-require("dotenv").config();
-
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
-client.messages
-  .create({
-    body: "Hello from Message Rooster",
-    from: process.env.TWILIO_FROM_NUMBER,
-    to: "+15558675309"
-  })
-  .then(message => console.log(message.sid));
+  const notifyAdmins = ({ messageBody }) => {
+    console.log("notify admins", process.env.ADMINS);
+
+    process.env.ADMINS.split(',').forEach(function(adminNumber) {
+      client.messages
+        .create({
+          body: `New message to Unspecified received: '${messageBody}'`,
+          from: process.env.TWILIO_FROM_NUMBER,
+          to: adminNumber,
+        })
+        .then((message) => console.log(message.status, message.sid))
+        .done();
+    })
+  };
+
+  module.exports = {
+    notifyAdmins,
+  }
